38d4df96a4d29f5ea3890e56263fd8e53e240092
PgmAdmin <prmorais1302@gmail.com>
2023-08-27 12:03:01 -0300

    Create components, interface and service for Associate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AssociateListingComponent } from './component/associate-listing/associate-listing.component';
+
+const routes: Routes = [{ path: '', component: AssociateListingComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AssociateListingComponent } from './component/associate-listing/associate-listing.component';
+import { AssociateComponent } from './component/associate/associate.component';
+import { MaterialModule } from './shared/material.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, AssociateListingComponent, AssociateComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
